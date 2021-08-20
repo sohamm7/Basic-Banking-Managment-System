f702d42d5b46e2d944494d3bc36bee1ac3758976
Soham Pradhan <72279094+sohamm7@users.noreply.github.com>
2021-08-20 21:24:41 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+	$server="localhost";
+	$username="root";
+	$password="";
+	$db="sparks_bank";
+	
+	$conn=mysqli_connect($server,$username,$password,$db);
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
